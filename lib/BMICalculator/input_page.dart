083acf188b2ccd6e940d24bd0ef5221a4ad0bf47
MukhammedAli <190103412@stu.sdu.edu.kz>
2021-10-24 16:29:35 +0600

    Refactoring code and adding containers

--- lib/BMICalculator/input_page.dart
+++ lib/BMICalculator/input_page.dart
@@ -14,9 +14,66 @@ class _InputPageState extends State<InputPage> {
         title: const Text('BMI Calculator'),
         backgroundColor: const Color(0xFF0A0E21),
       ),
-      body: const Center(
-        child: Text('Some text'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              child: Row(children: <Widget>[
+            Expanded(
+              child: ReusableCard(
+                color: const Color(0xFF1D1E33),
+              ),
+            ),
+            Expanded(
+              child: ReusableCard(
+                color: const Color(0xFF1D1E33),
+              ),
+            ),
+          ])),
+          Expanded(
+            child: Container(
+              margin: const EdgeInsets.all(15.0),
+              decoration: BoxDecoration(
+                color: const Color(0xFF1D1E33),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              height: 200.0,
+              width: double.infinity,
+            ),
+          ),
+          Expanded(
+              child: Row(children: <Widget>[
+            Expanded(
+              child: ReusableCard(
+                color: const Color(0xFF1D1E33),
+              ),
+            ),
+            Expanded(
+              child: ReusableCard(
+                color: const Color(0xFF1D1E33),
+              ),
+            ),
+          ])),
+        ],
       ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.color});
+
+  Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      height: 200.0,
+      width: 170.0,
     );
   }
 }
